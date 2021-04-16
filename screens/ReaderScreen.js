f2eb203888ea2ca99b3dd2e0da1740652c933190
mike paraskevopoulos <45458903+MikePrs@users.noreply.github.com>
2021-04-16 14:53:29 +0300

    Update ReaderScreen.js

--- screens/ReaderScreen.js
+++ screens/ReaderScreen.js
@@ -3,9 +3,9 @@ import { StyleSheet, View, FlatList, ActivityIndicator } from 'react-native';
 import { Content, Card, CardItem, Thumbnail, Text, Body, Left } from 'native-base';
 
 
-const key = "ce948e4581baccf7e7b2151e5916c041";
-const token = "6e3e3274ab75bfbc3bbecdce28e80e62edb2a373f82b93005cc42fc8069c8f40";
-const board_id = "Y66vXZ3l";
+const key = "";
+const token = "";
+const board_id = "";
 
 
 export default class ReaderScreen extends React.Component {
