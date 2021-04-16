eacafdfccbcf0657e7973924c97163fe67e28b53
mike paraskevopoulos <45458903+MikePrs@users.noreply.github.com>
2021-04-16 14:52:21 +0300

    Update AdminScreen.js

--- screens/AdminScreen.js
+++ screens/AdminScreen.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { StyleSheet, View, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 
 
-const key = "ce948e4581baccf7e7b2151e5916c041";
-const token = "6e3e3274ab75bfbc3bbecdce28e80e62edb2a373f82b93005cc42fc8069c8f40";
-const board_id = "Y66vXZ3l";
+const key = "";
+const token = "";
+const board_id = "";
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Right, Body, Left } from 'native-base';
 
 export default class AdminScreen extends React.Component {
